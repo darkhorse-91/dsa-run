fce80a5a4eba60fcb74d78ada3ad4967f927c57b
Aniket Chaurasia <ac226018me@gmail.com>
2023-05-24 10:55:41 +0530

    starting linked list problems

---
+++ arrays/special-subsequences-ag.js
@@ -0,0 +1,10 @@
+// # Problem Description
+// # You have given a string A having Uppercase English letters.
+// # You have to find how many times subsequence "AG" is there in the given string.
+// # NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+
+// # Input 1: ABCGAG
+// # Output 1 : 3
+// # Input 2: GAB
+// # Output 2: 0
+
